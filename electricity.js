5516404ae0cf00ff0120d8160ed3313914d2f138
Michael Jonker <michael@openpoint.ie>
2018-11-02 08:24:33 +0000

    Hook for loops in main

--- electricity.js
+++ electricity.js
@@ -1,2 +1,11 @@
 "use strict";
-require("./lib/init.js");
+const {init} = require("./lib/init.js");
+const {Log} = require("./lib/globals");
+
+init((err)=>{
+	if(err){
+		Log.error("Shutting down, check the logs for more details");
+		process.exit();
+	}
+	console.log("Starting the loops");
+})
